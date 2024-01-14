b80fb2f83ba92bbfa33731728c46099bcea7ff2c
leandre <tuyambazeleandre@gmail.com>
2024-01-14 18:10:13 +0200

    ch(hound-ci): add hound ci for automatic review

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ const number1: number = 5;
 const number2: number = 10;
 const newNumber = 8;
 const result: number = addition(number1, number2);
-// console.log(result)
+// console.log(result);
 
 console.error('result', result, hello, newNumber);
