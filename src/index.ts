2ee3bffd1b695740fee9b32bc2c6a7a3650f8815
leandre <tuyambazeleandre@gmail.com>
2024-01-14 14:47:32 +0200

    ch(linting):implenting linting rules and add pre-commit with husky
    ch(lint):add lint rules
    
    ch(lint):add lint rules

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 const addition = (a: number, b: number): number => {
   return a + b;
 };
-
+const hello = 10;
 const number1: number = 5;
 const number2: number = 10;
+const newNumber = 8;
 const result: number = addition(number1, number2);
+// console.log(result)
 
-console.log('The result is %d', result);
+console.error('result', result, hello, newNumber);
